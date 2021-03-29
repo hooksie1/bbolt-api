fd9d39dbee6df60219f36dbdf7cccb3c680cfb53
John Hooks <john@boxboat.com>
2021-03-29 19:49:06 -0400

    Fix name

--- server/admin.go
+++ server/admin.go
@@ -9,7 +9,7 @@ import (
 func backupDB(w http.ResponseWriter, r *http.Request) error {
 	err := db.View(func(tx *bbolt.Tx) error {
 		w.Header().Set("Content-Type", "application/octet-stream")
-		w.Header().Set("Content-Disposition", `attachment; filename="piggy-backup.db"`)
+		w.Header().Set("Content-Disposition", `attachment; filename="db-backup.db"`)
 		w.Header().Set("Content-Length", strconv.Itoa(int(tx.Size())))
 		_, err := tx.WriteTo(w)
 		return err
